4b40f95d04964089e0b0ee252c08f72365649f8a
chenghsienyu <chenghsienyu@aim.com>
2022-06-09 17:12:28 +0800

    update

--- examples/js/GSVPano.js
+++ examples/js/GSVPano.js
@@ -80,7 +80,8 @@ GSVPANO.PanoLoader = function (parameters) {
 	
 		for( y = 0; y < h; y++) {
 			for( x = 0; x < w; x++) {
-				url = 'http://maps.google.com/cbk?output=tile&panoid=' + panoId + '&zoom=' + _zoom + '&x=' + x + '&y=' + y + '&' + Date.now();
+				url = 'http://cbk0.google.com/cbk?output=tile&panoid=' + panoId + '&zoom=' + _zoom + '&x=' + x + '&y=' + y + '&' + Date.now();
+				// url = 'http:maps.google.com/cbk?output=tile&panoid=' + panoId + '&zoom=' + _zoom + '&x=' + x + '&y=' + y + '&' + Date.now();
 				(function (x, y) { 
 					var img = new Image();
 					img.addEventListener('load', function () {
